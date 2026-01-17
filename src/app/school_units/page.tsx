dac233eea3864d3b7f7f3806c5ffb36b0982cfb2
MarcioParanhos <marcio.naga@gmail.com>
2026-01-17 11:35:33 -0300

    :sparkles: feat: Adiciona modelo Typology ao schema Prisma e atualiza o modelo SchoolUnit para incluir relação com Typology.

--- src/app/school_units/page.tsx
+++ src/app/school_units/page.tsx
@@ -1,2 +1,10 @@
 "use client";
 
+export default function SchoolUnitsPage() {
+  return (
+    <div>
+      <h1>School Units</h1>
+      <p>Welcome to the School Units page.</p>
+    </div>
+  );
+}
